4c4fd67b33c8eaf9d6413bdac4282466efb62a60
Shirasaki_Hisako <shirasaki_hisako@Shirasaki-HisakonoMacBook-puro.local>
2018-10-11 20:53:05 +0900

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,10 +8,12 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import MyButton from '@components/Button.vue';
 
 @Component({
   components: {
     HelloWorld,
+    MyButton,
   },
 })
 export default class Home extends Vue {}
